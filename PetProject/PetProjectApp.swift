c04e35ea92930c8f654983d3f1a5d13a2818e88a
Nazmka <nazym-8@mail.ru>
2025-05-11 12:35:54 +0500

    Initial Commit

---
+++ PetProject/PetProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetProjectApp.swift
+//  PetProject
+//
+//  Created by Nazm M on 11.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct PetProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
